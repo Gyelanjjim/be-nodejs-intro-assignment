7301f012a92381b417f07c87ce3e536b1c7262c3
Gyelanjjim <songcj92@gmail.com>
2023-02-02 18:59:50 +0900

    [Update] filter() -> trim()

--- main3.js
+++ main3.js
@@ -1,37 +1,42 @@
-const fileSystem = require("fs"); 
-const util = require('util')
+const fileSystem = require("fs");
+const util = require("util");
 
 const users = fileSystem.readFileSync("./users.csv", "utf-8");
 const posts = fileSystem.readFileSync("./posts.csv", "utf-8");
 
 const csvToObject = (csv) => {
-    csv = csv.replace(/\r/g,"")     // users에 있는 \r을 제거
-    csv = csv.replace(/, /g,",")    // posts에 있는 ", "을 ","로 변경
-    const rows = csv.split("\n")
-        .filter(el => el !== "")    // posts에 있는 ""을 제거
-        .map(el => el.split(",")) 
-    const result = []
-    for(let i = 1; i<rows.length; i++){
-        const obj = {}
-        for(let j = 0; j<rows[0].length; j++){
-            obj[rows[0][j]] = rows[i][j]
-        }
-        result.push(obj)    
+  csv = csv.replace(/\r/g, ""); // users에 있는 \r을 제거
+  csv = csv.replace(/, /g, ","); // posts에 있는 ", "을 ","로 변경
+  const rows = csv
+    .trim() // 양옆의 공백, 개행문자 삭제
+    .split("\n")
+    .map((el) => el.split(","));
+  const result = [];
+  for (let i = 1; i < rows.length; i++) {
+    const obj = {};
+    for (let j = 0; j < rows[0].length; j++) {
+      obj[rows[0][j]] = rows[i][j];
     }
-    return result
-}
+    result.push(obj);
+  }
+  return result;
+};
 
 const postsByUsers = (postsArr, usersArr) => {
-    for(let elU of usersArr){
-        elU.posts = []
-        for(let elP of postsArr){
-            if(elU.id === elP.user_id) elU.posts.push(elP)
-        }
+  for (let elU of usersArr) {
+    elU.posts = [];
+    for (let elP of postsArr) {
+      if (elU.id === elP.user_id) elU.posts.push(elP);
     }
-    return util.inspect(usersArr, {showHidden: false, depth: null, colors: true})
-}
+  }
+  return util.inspect(usersArr, {
+    showHidden: false,
+    depth: null,
+    colors: true,
+  });
+};
 
-const usersArr = csvToObject(users)
-const postsArr = csvToObject(posts)
+const usersArr = csvToObject(users);
+const postsArr = csvToObject(posts);
 
-console.log(postsByUsers(postsArr, usersArr))
+console.log(postsByUsers(postsArr, usersArr));
